355223a2ceb50ef8fc814f6fd4b1cb656e934bee
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2022-03-01 16:44:23 +0530

    created user-profile section

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //declarations
 const sidebarToggle = document.getElementById('sidebarToggle')
 const navLinks = document.querySelectorAll('.nav-link')
-
+const logout = document.getElementById('logout')
 
 
 //Event Listener functions
@@ -14,21 +14,49 @@ sidebarToggle.addEventListener('click',(event)=>{
 })
 
 // header modal toggle 
+modalState = {}
 linkToModelMap = {}
 navLinks.forEach((link,linkIndex)=>{
      const left = link.parentNode.parentNode.getBoundingClientRect().x
-     const right = link.parentNode.parentNode.getBoundingClientRect().y
-     console.log(left,right);    
+     const top = link.parentNode.parentNode.getBoundingClientRect().y
+     console.log(left,top);    
      linkToModelMap[linkIndex] = link.href.split('#')[1]
+     modalState[linkIndex] = false
      link.removeAttribute('href')
      link.addEventListener('click',(event)=>{
-          event.preventDefault()
+          Object.keys(linkToModelMap).filter((el)=>Number(el)!==linkIndex).map((key)=>{
+               const modalId = linkToModelMap[key]
+              
+         const modal = document.getElementById(modalId)
+         modal.classList.remove('modal-open')
+          })
+     event.preventDefault()
           const modalId = linkToModelMap[linkIndex]
          const modal = document.getElementById(modalId)
-         modal.style.left = `${left-100}px`
-         modal.style.top = `${right}px`
-         modal.classList.toggle('modal-open')
-        
+         modal.style.left = `${left-50}px`
+         modal.style.top = `${top+30}px`
+         if(modalState[linkIndex] == true){
+              modal.classList.remove('modal-open')
+              
+         }
+         else{
+          modal.classList.add('modal-open')
+         
+         }
+         modalState[linkIndex] = !modalState[linkIndex]
+         console.log(linkToModelMap,modalState,linkIndex);
      })
 
+})
+
+//them toggle
+const themeToggleBtn = document.querySelector('.toggle-theme-icon')
+themeToggleBtn.addEventListener('click',(event)=>{
+     const icon = event.target
+     icon.innerText=="light_mode"? icon.innerText ='dark_mode':icon.innerText ='light_mode'
+})
+
+//logout
+logout.addEventListener('click',(event)=>{
+     
 })
